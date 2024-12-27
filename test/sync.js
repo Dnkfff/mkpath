6ea3c62e9a6490db638525845d582fd396bcf8c4
Dnkfff <ddynikov@gmail.com>
2024-12-27 12:05:31 +0200

    actions disable

--- test/sync.js
+++ test/sync.js
@@ -1,35 +1,35 @@
-// import mkpath from '../mkpath.js';
-// import { stat as _stat } from 'fs';
-// import { test } from 'tap';
+import mkpath from '../mkpath.js';
+import { stat as _stat } from 'fs';
+import { test } from 'tap';
 
-// test('sync', (t) => {
-//   t.plan(2);
-//   const x = Math.floor(Math.random() * 0x10000).toString(16);
-//   const y = Math.floor(Math.random() * 0x10000).toString(16);
-//   const z = Math.floor(Math.random() * 0x10000).toString(16);
+test('sync', (t) => {
+  t.plan(2);
+  const x = Math.floor(Math.random() * 0x10000).toString(16);
+  const y = Math.floor(Math.random() * 0x10000).toString(16);
+  const z = Math.floor(Math.random() * 0x10000).toString(16);
 
-//   const file = `/tmp/${x}/${y}/${z}`;
+  const file = `/tmp/${x}/${y}/${z}`;
 
-//   try {
-//     mkpath.sync(file, 0o755); // Using the 0o755 mode
-//   } catch (err) {
-//     t.fail(err);
-//     return t.end();
-//   }
+  try {
+    mkpath.sync(file, 0o755); // Using the 0o755 mode
+  } catch (err) {
+    t.fail(err);
+    return t.end();
+  }
 
-//   _stat(file, (err, stat) => {
-//     if (err) t.fail(err);
-//     else {
-//       // Extract the actual mode
-//       const actualMode = stat.mode & 0o777;
-//       const expectedMode = 0o666;
+  _stat(file, (err, stat) => {
+    if (err) t.fail(err);
+    else {
+      // Extract the actual mode
+      const actualMode = stat.mode & 0o777;
+      const expectedMode = 0o666;
 
-//       // Ensure the actual mode includes the permissions of 0o755
-//       // The actual mode must be at least 0o755, but can have additional restrictions due to the umask
-//       t.ok((actualMode & expectedMode) === expectedMode, `Directory mode includes at least expected permissions (got ${actualMode}, expected ${expectedMode})`);
+      // Ensure the actual mode includes the permissions of 0o755
+      // The actual mode must be at least 0o755, but can have additional restrictions due to the umask
+      t.ok((actualMode & expectedMode) === expectedMode, `Directory mode includes at least expected permissions (got ${actualMode}, expected ${expectedMode})`);
 
-//       t.ok(stat.isDirectory(), 'Target is a directory');
-//     }
-//     t.end();
-//   });
-// });
+      t.ok(stat.isDirectory(), 'Target is a directory');
+    }
+    t.end();
+  });
+});
